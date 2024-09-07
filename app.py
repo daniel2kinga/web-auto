5d9da1503c3e2da0481cd3ff4484137eff9e38d8
daniel2kinga <daniel2kinga@yahoo.com>
2024-09-07 23:07:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
 app = Flask(__name__)
+
+# Ruta para la raíz '/'
+@app.route('/')
+def home():
+    return "Bienvenido a mi API. Usa la ruta /extraer para extraer contenido web."
 
 # Configurar Selenium para Chrome en modo headless
 def configurar_driver():
@@ -14,17 +19,17 @@ def configurar_driver():
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
-# Ruta para extraer el contenido de una página web
+# Ruta para extraer contenido de una página web
 @app.route('/extraer', methods=['POST'])
 def extraer_pagina():
     data = request.json
-    url = data.get('https://parceladigital.com/articulos')
+    url = data.get('url')
     if not url:
         return jsonify({"error": "No se proporcionó URL"}), 400
 
     driver = configurar_driver()
     driver.get(url)
-    time.sleep(5)  # Esperar a que cargue la página
+    time.sleep(5)
 
     contenido = driver.find_elements_by_tag_name("p")
     texto_extraido = " ".join([element.text for element in contenido])
