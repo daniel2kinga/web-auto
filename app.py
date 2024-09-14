3278821079d481c909765a544c2e3d1fd1d07348
daniel2kinga <daniel2kinga@yahoo.com>
2024-09-14 13:19:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 def configurar_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Puedes quitar esto si quieres ver la automatización en pantalla
+    chrome_options.add_argument("--headless")  # Ejecuta en modo headless (sin interfaz gráfica)
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
