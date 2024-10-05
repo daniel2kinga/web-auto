0ff73efc0119e3cc612a0403fd5c5c125e4bb716
daniel2kinga <daniel2kinga@yahoo.com>
2024-10-05 16:39:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import time
 import logging
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
