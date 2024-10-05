c28b498cf36688f4890257d94f5b35d8ddabdc3a
daniel2kinga <daniel2kinga@yahoo.com>
2024-10-05 23:04:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def configurar_driver():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--remote-debugging-port=9222")  # Necesario para Railway
     chrome_options.add_argument("--disable-cache")  # Desactivar caché
-
+chrome_options.add_argument("--headless")
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     return driver
