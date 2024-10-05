66eb7bf0ee27f9f2f0cd6564308bb8ce27b0bf2f
daniel2kinga <daniel2kinga@yahoo.com>
2024-10-05 19:25:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -195,4 +195,8 @@ def extraer_pagina():
         logger.error(f"Error al procesar la solicitud: {e}")
         return jsonify({"error": "Error interno del servidor", "details": str(e)}), 500
 
-# No incluimos el bloque if __name__ == '__main__' para permitir que Gunicorn lo maneje
+# Iniciar la aplicación Flask
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
