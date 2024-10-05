e410bf0b5678a81a87383c594c5c3adb90accd91
daniel2kinga <daniel2kinga@yahoo.com>
2024-10-05 22:07:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,11 @@
 # Resolver la variable de entorno $PORT
 PORT=${PORT:-5000}
 
+# Verificar si $PORT es un número válido
+if ! [[ "$PORT" =~ ^[0-9]+$ ]]; then
+  echo "Error: '$PORT' is not a valid port number."
+  exit 1
+fi
+
 # Ejecutar gunicorn
 exec gunicorn app:app --bind 0.0.0.0:$PORT
