4f6198184e190ed5af00274393a1c0878e865a67
daniel2kinga <daniel2kinga@yahoo.com>
2024-10-20 13:02:05 +0200

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
-# Descargar Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# Instalar dependencias necesarias
+apt-get update && apt-get install -y \
+    wget \
+    unzip \
+    fontconfig \
+    locales \
+    libx11-6 \
+    libxext6 \
+    libxrender1 \
+    libxss1 \
+    libxtst6 \
+    xfonts-100dpi \
+    xfonts-75dpi \
+    xfonts-scalable \
+    xfonts-cyrillic \
+    fonts-liberation \
+    libappindicator1 \
+    libnss3 \
+    lsb-release \
+    xdg-utils \
+    libgtk-3-0 \
+    libx11-xcb1 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxi6 \
+    libxtst6 \
+    libxrandr2 \
+    libasound2 \
+    libpangocairo-1.0-0 \
+    libpango1.0-0 \
+    libcairo2 \
+    libffi-dev \
+    libssl-dev \
+    python3-dev
 
-# Instalar Chrome
-apt-get install -y ./google-chrome-stable_current_amd64.deb
-
-# Descargar ChromeDriver
-CHROME_VERSION=$(google-chrome --version | grep -oP '\d{2,3}')
-DRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-wget https://chromedriver.storage.googleapis.com/$DRIVER_VERSION/chromedriver_linux64.zip
-
-# Extraer ChromeDriver
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-mv chromedriver /usr/local/bin/
+# Descargar e instalar Google Chrome
+wget -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
+    && apt-get install -y /tmp/chrome.deb \
+    && rm /tmp/chrome.deb
